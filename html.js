e972205343ba7639b2f35ebdbb389d2cda7383c7
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-12-21 22:18:20 +0900

    Initial commit

---
+++ html.js
@@ -0,0 +1,17 @@
+//変数
+let Hello = 'Hello World!';
+
+//定数
+const bigUnko = 'He..Hell...Hello World';
+
+//配列
+const inoki = ['いーち','にー','さーん','ダー!！'];
+
+//ループ文
+let index = 0;
+while(index < 5){
+  //繰り返したい命令
+  console.log(index);
+  index++; //index=5
+}
+console.log(Hello);
